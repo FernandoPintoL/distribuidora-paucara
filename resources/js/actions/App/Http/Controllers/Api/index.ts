38750458bcc08e380705faf14ea6f4cf845fa710
Fernando Pinto Lino <30969689+FernandoPintoL@users.noreply.github.com>
2025-10-28 14:10:29 -0400

    correcion api resources

--- resources/js/actions/App/Http/Controllers/Api/index.ts
+++ resources/js/actions/App/Http/Controllers/Api/index.ts
@@ -1,11 +1,13 @@
 import AuthController from './AuthController'
 import EmpleadoApiController from './EmpleadoApiController'
+import RoleController from './RoleController'
 import TipoMermaController from './TipoMermaController'
 import EstadoMermaController from './EstadoMermaController'
 import ApiProformaController from './ApiProformaController'
 const Api = {
     AuthController,
 EmpleadoApiController,
+RoleController,
 TipoMermaController,
 EstadoMermaController,
 ApiProformaController,
